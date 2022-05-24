42e4ecb1f307e406a53f08edce88b92b25684f75
ChaitanyaChandra <chay@outlook.in>
2022-05-25 00:29:45 +0530

    automated commit #170

---
+++ cmd.sh
@@ -0,0 +1,9 @@
+grep -rnw '/path/to/somewhere/' -e 'pattern'
+git pull ; ansible-playbook roboshop.yml  \
+  -e ENV=dev -e GIT_DOWNLOAD=yes \
+  -t SHIPPING -f 30 -i roboshop.inv
+# -f fork running, parallel
+# -t tags
+# -e env var
+# -i inventory
+
