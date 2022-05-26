3d3a3fdb5b57c82c2aa51c3e60270c2fe3e24c35
ChaitanyaChandra <chay@outlook.in>
2022-05-26 14:57:37 +0530

    automated commit #184

---
+++ roles/jenkins/templates/admin_setup.groovy
@@ -0,0 +1,19 @@
+/*
+ * Create an admin user.
+ */
+import jenkins.model.*
+import hudson.security.*
+
+println "--> creating admin user"
+
+def adminUsername = "{{ USER_NAME }}"
+def adminPassword = "{{ PASSWORD }}"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(adminUsername, adminPassword)
+Jenkins.instance.setSecurityRealm(hudsonRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+Jenkins.instance.setAuthorizationStrategy(strategy)
+
+Jenkins.instance.save()
