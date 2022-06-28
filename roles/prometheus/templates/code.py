a0e552aaf65ea44bdda178f0eaa46bed7e368437
ChaitanyaChandra <chay@outlook.in>
2022-06-28 18:32:11 +0530

    automated commit #966

--- roles/prometheus/templates/code.py
+++ roles/prometheus/templates/code.py
@@ -5,4 +5,13 @@ DOMAIN = "{{DOMAIN}}"
 new_list = []
 for APP in APPS:
     new_list.append(f"{APP}-{ENV}-{DOMAIN}:9100")
-print(new_list)
+append_string = f"""
+  - job_name: "nodes"
+    static_configs:
+      - targets: {new_list}
+"""
+
+# Open a file with access mode 'a'
+with open("/opt/prometheus/prometheus.yml", "a") as file_object:
+    # Append 'hello' at the end of file
+    file_object.write(append_string)
