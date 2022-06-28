6136daaf810c91f66a85b46a091a71b03ff9f20b
ChaitanyaChandra <chay@outlook.in>
2022-06-28 11:12:47 +0530

    automated commit #937

--- roles/prometheus/templates/code.py
+++ roles/prometheus/templates/code.py
@@ -1,7 +1,7 @@
 APPS = {{ APPS | to_json }}
 
-ENV = {{ENV}}
-DOMAIN = {{DOMAIN}}
+ENV = "{{ENV}}"
+DOMAIN = "{{DOMAIN}}"
 new_list = []
 for APP in APPS:
     new_list.append(f"{APP}-{ENV}-{DOMAIN}:9100")
