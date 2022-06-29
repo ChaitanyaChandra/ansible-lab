0308a515e81bd179219a2689f3769361cd5278e8
ChaitanyaChandra <chay@outlook.in>
2022-06-29 11:01:54 +0530

    automated commit #994

--- roles/prometheus/templates/code.py
+++ roles/prometheus/templates/code.py
@@ -6,10 +6,11 @@ DOMAIN = "{{DOMAIN}}"
 new_list = []
 for APP in APPS:
     new_list.append(f'{APP}-{ENV}-{DOMAIN}:9100')
-    with open("/opt/prometheus/prometheus.yml", "a") as file_object:
-        file_object.write(f"""
-  - job_name: "{APP}"
-    static_configs:
-      - targets: ["{APP}-{ENV}.{DOMAIN}:9100"]
-""")
 print(json.dumps(new_list))
+
+with open("/opt/prometheus/prometheus.yml", "a") as file_object:
+    file_object.write(f"""
+  - job_name: "node"
+    static_configs:
+      - targets: {json.dumps(new_list)}
+""")
