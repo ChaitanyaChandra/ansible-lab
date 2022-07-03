b25899dfd632b8ab6f10313b6abbb8bd74fee3e4
ChaitanyaChandra <chay@outlook.in>
2022-07-03 12:16:05 +0530

    automated commit #1096

--- roles/elk/files/code.sh
+++ roles/elk/files/code.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -s "https://raw.githubusercontent.com/majorchowdary/scripts/master/common-functions.sh" >/tmp/common-functions.sh
 #source /root/scripts/common-functions.sh
 source /tmp/common-functions.sh
